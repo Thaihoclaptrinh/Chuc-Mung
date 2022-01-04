0686dee86f75f78f256bc0d13a1f36d00d48ecd8
Thaihoclaptrinh <97115687+Thaihoclaptrinh@users.noreply.github.com>
2022-01-04 23:26:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc em sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau . Phải thật hạnh phúc nhé';
+const contentCard = 'Chúc em sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Phải thật hạnh phúc nhé';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
