406c404209560f5e0da24d879f136188869a3f4e
Thaihoclaptrinh <97115687+Thaihoclaptrinh@users.noreply.github.com>
2022-03-08 14:06:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Mẹ';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mẹ Ngày 8-3';
 const titleCard = 'Tặng Mẹ';
-const contentCard = 'Chúc mẹ có một ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với mẹ trong hôm nay và cả những ngày sau. Con cảm ơn mẹ đã luôn chăm sóc và dạy dỗ hai anh em con';
+const contentCard = 'Chúc mẹ có một ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với mẹ trong hôm nay và cả những ngày sau. Con cảm ơn mẹ đã luôn chăm sóc và dạy dỗ hai anh em con. Chúc mừng mẹ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
