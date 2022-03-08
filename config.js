ca9e3235c7a24dd92a0efa1e1de142a9f18bddb6
Thaihoclaptrinh <97115687+Thaihoclaptrinh@users.noreply.github.com>
2022-03-08 13:24:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Bạn';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng Ngày 8-3';
-const titleCard = 'Tặng Vợ Tương Lai';
-const contentCard = 'Chúc em có một ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Phải thật hạnh phúc nhé. Yêu em nhất thế giới';
+const titleCard = 'Tặng Bạn';
+const contentCard = 'Chúc bạn có một ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau. Phải thật thành công nhé';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
